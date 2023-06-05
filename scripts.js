0756d41a53de77a758745cca5481c58c2dc9f922
Genevieve Latimer <genevievelatimer@gmail.com>
2023-06-05 10:08:50 -0700

    I was able to add js to the navbar for the index.html.  Next I will attempt to customize the navbar js to change link paths relative to the current page.

---
+++ scripts.js
@@ -0,0 +1,39 @@
+// Append the navbar to the top of each page
+
+// Create a function to generate the navbar HTML
+function buildNavbar() {
+  return `
+    <img
+      class="GL"
+      src="../images/logo.png"
+      alt="Genevieve's initials in her own handwriting.  White lettering with dusty blue background."
+    />
+    <h1>Genevieve Latimer</h1>
+    <nav>
+      <ul class="nav-list">
+        <li class="nav-button">
+          <a class="nav-click" href="#project">Home</a>
+        </li>
+        <li class="nav-button">
+          <a class="nav-click" href="about-me.html">About Me</a>
+        </li>
+        <li class="nav-button">
+          <a class="nav-click" href="experience.html">Experience</a>
+        </li>
+        <li class="nav-button">
+          <a class="nav-click" href="contact-info.html">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  `;
+}
+
+// Append the navbar HTML to the header element
+function appendNavbar() {
+  const header = document.querySelector("header");
+  const navbarHTML = buildNavbar();
+  header.innerHTML = navbarHTML;
+}
+
+// Call the appendNavbar function when the DOM is loaded
+document.addEventListener("DOMContentLoaded", appendNavbar);
